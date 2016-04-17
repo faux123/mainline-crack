571555b7ed8364c16fb4e09bad0b9dea3d1b15b4
Paul Reioux <reioux@gmail.com>
2016-04-17 16:30:06 -0700

    Revert "The Brain Fuck Scheduler v0.469 by Con Kolivas."
    This reverts commit 0a1dcc77c71bcd172b245c298cbcbddf7d78b17a.

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -575,13 +575,8 @@ static void atom_set_pstate(struct cpudata *cpudata, int pstate)
 	vid_fp = clamp_t(int32_t, vid_fp, cpudata->vid.min, cpudata->vid.max);
 	vid = ceiling_fp(vid_fp);
 
-	if (pstate < cpudata->pstate.max_pstate)
-		cpu_scaling(cpudata->cpu);
-	else {
-		if (pstate > cpudata->pstate.max_pstate)
-			vid = cpudata->vid.turbo;
-		cpu_nonscaling(cpudata->cpu);
-	}
+	if (pstate > cpudata->pstate.max_pstate)
+		vid = cpudata->vid.turbo;
 
 	val |= vid;
 
